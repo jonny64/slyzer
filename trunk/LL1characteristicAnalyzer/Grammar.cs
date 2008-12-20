34f7906037bc069d74dd710a50915d074e78ac5b
astro64m <astro64m@071d4850-ceb8-11dd-bc61-0ff7aae23933>
2008-12-20 19:03:11 +0000

    implemented base grammar class

---
+++ trunk/LL1characteristicAnalyzer/Grammar.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL1characteristicAnalyzer
+{
+    class Grammar
+    {
+        public Grammar(string[] productions, string[] terminalWords)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Production> grammar
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
