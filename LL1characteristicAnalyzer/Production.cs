2a649df6d62206076fc2af637889c1c08b6f9df1
astro64m <astro64m@071d4850-ceb8-11dd-bc61-0ff7aae23933>
2008-12-20 19:03:11 +0000

    implemented base grammar class

---
+++ LL1characteristicAnalyzer/Production.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LL1characteristicAnalyzer
+{
+    class Production
+    {
+        public Production(string[] prodString)
+        {
+            throw new System.NotImplementedException();
+        }
+    
+        public Symbol Head
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public List<Symbol> Tail
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
