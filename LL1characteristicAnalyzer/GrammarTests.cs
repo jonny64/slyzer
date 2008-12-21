e66630da702dd9981fe3ef42c808ba225c090c09
astro64m <astro64m@071d4850-ceb8-11dd-bc61-0ff7aae23933>
2008-12-21 19:37:06 +0000

    implemented first set evaluation

--- LL1characteristicAnalyzer/GrammarTests.cs
+++ LL1characteristicAnalyzer/GrammarTests.cs
@@ -8,8 +8,29 @@ namespace LL1AnalyzerTool
     [TestFixture]
     public class GrammarTests
     {
+        private Grammar grammar;
+
+        [SetUpAttribute]
+        public void Init()
+        {
+            string[] productions = { "S A B C",
+                "A D E",
+                "B F G",
+                "C #",
+                "D a",
+                "D #",
+                "E a a",
+                "E #",
+                "F H K",
+                "G b b",
+                "H c c",
+                "K d d"
+            };
+            grammar = new Grammar(productions);
+        }
+
         [Test]
-        public void TestEmptySet()
+        public void EmptySet()
         {
             string[] productions = { "S A B C",
                 "A D E",
@@ -49,10 +70,44 @@ namespace LL1AnalyzerTool
                                              Grammar.EmptyState.NON_EMPTY,
                                          };
             for (int i = 0; i < syms.Length; i++)
-			{
-			    Grammar.EmptyState actual = grammar.GetEmptyHashtable()[syms[i]];
+            {
+                Grammar.EmptyState actual = grammar.GetEmptyHashtable()[syms[i]];
                 Assert.AreEqual(actual, empty[i]);
-			}
+            }
+        }
+
+        [Test]
+        public void FirstSet()
+        {
+            Symbol[] syms = {new Symbol("S"),
+                new Symbol("A"),
+                new Symbol("B"),
+                new Symbol("C"),
+                new Symbol("D"),
+                new Symbol("E"),
+                new Symbol("F"),
+                new Symbol("G"),
+                new Symbol("H"),
+                new Symbol("K")
+            };
+            Set[] firsts = {
+                    new Set(new Symbol("a"), new Symbol("c")),
+                    new Set(new Symbol("a")),
+                    new Set(new Symbol("c")),
+                    new Set(),
+                    new Set(new Symbol("a")),
+                    new Set(new Symbol("a")),
+                    new Set(new Symbol("c")),
+                    new Set(new Symbol("b")),
+                    new Set(new Symbol("c")),
+                    new Set(new Symbol("d"))
+                           };
+            for (int i = 0; i < syms.Length; i++)
+            {
+                Set actual = grammar.First(syms[i]);
+                Set difference = firsts[i]/actual;
+                Assert.AreEqual(0, difference.Count);
+            }
         }
     }
 }
