a300ccd06f8d5ca03cc7aeb6b093d017005f48cd
astro64m <astro64m@071d4850-ceb8-11dd-bc61-0ff7aae23933>
2008-12-22 18:12:44 +0000

    implemented dir syms tests

--- LL1characteristicAnalyzer/GrammarTests.cs
+++ LL1characteristicAnalyzer/GrammarTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LL1AnalyzerTool
@@ -33,20 +34,8 @@ namespace LL1AnalyzerTool
         [Test]
         public void EmptySet()
         {
-            string[] productions = { "S A B C",
-                "A D E",
-                "B F G",
-                "C #",
-                "D a",
-                "D #",
-                "E a a",
-                "E #",
-                "F H K",
-                "G b b",
-                "H c c",
-                "K d d"
-            };
-            Grammar grammar = new Grammar(productions);
+            string testfile = "Grammars\\test1.txt";
+            Grammar grammar = Grammar.LoadFromFile(testfile);
             Symbol[] syms = {new Symbol("S"),
                 new Symbol("A"),
                 new Symbol("B"),
@@ -149,29 +138,55 @@ namespace LL1AnalyzerTool
         }
 
         [Test]
-        public void DirSymbolsSimpleSet()
+        public void DirSymbolsSimpleTest()
         {
-            string[] productionsSimple = { "S a S b",
-                "S a S c",
-                "S #"
-            };
-            Grammar simpleGrammar = new Grammar(productionsSimple);
+            string[] grFilesList = { "test1.txt", "test2.txt", "test3.txt" };
+            string[] dirSymsFilesList = { "set1.txt", "set2.txt", "set3.txt" };
 
-            Set[] dirSyms = {
-                    new Set(new Symbol("a")),
-                    new Set(new Symbol("a")),
-                    new Set(new Symbol("b"),  new Symbol("c"))
-                            };
-            for (int i = 0; i < simpleGrammar.Length; i++)
+            for (int grFile = 0; grFile < grFilesList.Length; grFile++)
+			{
+                Grammar simpleGrammar = Grammar.LoadFromFile("Grammars\\" + grFilesList[grFile]);
+
+                Set[] dirSyms = LoadDirectionSymsFromFile("DirectionSets\\" + dirSymsFilesList[grFile]);
+
+                for (int i = 0; i < simpleGrammar.Length; i++)
+                {
+                    Set actual =
+                        simpleGrammar.GetDirectionSymbols(simpleGrammar.GetProduction(i).ToLinkedList());
+                    Set difference = dirSyms[i] / actual;
+                    Assert.AreEqual(0, difference.Count,
+                        String.Format("Grammar {3}; Production {0}; actual set {1}; expected {2};",
+                            simpleGrammar.GetProduction(i), actual, dirSyms[i], grFilesList[grFile])
+                        );
+                } 
+			}
+        }
+
+        private Set[] LoadDirectionSymsFromFile(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            List<Set> sets = new List<Set>();
+            while (sr.Peek() != -1)
             {
-                Set actual =
-                    simpleGrammar.GetDirectionSymbols(simpleGrammar.GetProduction(i).ToLinkedList());
-                Set difference = dirSyms[i] / actual;
-                Assert.AreEqual(0, difference.Count,
-                    String.Format("Production: {0}, actual set: {1}, expected: {2}",
-                        simpleGrammar.GetProduction(i), actual, dirSyms[i])
-                    );
+                string line = sr.ReadLine();
+                if (line[0] == ';')
+                {
+                    continue;
+                }
+                if (line == "\n")
+                {
+                    break;
+                }
+
+                string[] syms = line.Split(' ');
+                Set set = new Set();
+                foreach (string sym in syms)
+                {
+                    set.Add(new Symbol(sym));
+                }
+                sets.Add(set);
             }
+            return sets.ToArray();
         }
     }
 }
