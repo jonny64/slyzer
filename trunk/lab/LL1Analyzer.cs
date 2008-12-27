54b13c85e33ce556f1646581e72681e50359e456
astro64m <astro64m@071d4850-ceb8-11dd-bc61-0ff7aae23933>
2008-12-27 13:52:58 +0000

    adapted LL1Analyzer to use new ParseTable generated from grammar file

--- trunk/lab/LL1Analyzer.cs
+++ trunk/lab/LL1Analyzer.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using LL1AnalyzerTool;
 namespace lab
 {
     class LL1Analyzer
@@ -13,67 +13,14 @@ namespace lab
         //лексический анализатор - "подносчик патронов"
         Lexan myParser;
 
-        public string exp;
+        public string program;
         int symIndex = 0;
-        public struct TableRow
-        {
-            public char[] terminals;
-            public int jump;
-            public bool accept, stack, error;
-            public TableRow(char[] terms, int jmp, bool accpt,bool stck, 
-                                                    bool err)
-            {
-                terminals=terms;
-                jump=jmp;
-                accept=accpt;stack=stck;error=err;
-                //else errorMsg = "<не установлен текст для ошибки>";
-            }
-                
-        }
         //ТАБЛИЦА РАЗБОРА
-        TableRow[] parsTable ={
-new TableRow( new char[] {                  'r'},	1,	false,	false,	true),
-new TableRow( new char[] {                  'r'},	2,	true,	false,	true),
-new TableRow( new char[] {                  'i'},	4,	false,	true,	true),
-new TableRow( new char[] {                  'e'},	34,	true,	false,	true),
-new TableRow( new char[] {                  'i'},	5,	false,	false,	true),
-new TableRow( new char[] {                  'i'},	13,	false,	true,	true),
-new TableRow( new char[] {             's', 'e'},	7,	false,	false,	true),
-new TableRow( new char[] {                  's'},	9,	false,	false,	false),
-new TableRow( new char[] {                  'e'},	12,	false,	false,	true),
-new TableRow( new char[] {                  's'},	10,	true,	false,	true),
-new TableRow( new char[] {                  'i'},	13,	false,	true,	true),
-new TableRow( new char[] {             's', 'e'},	7,	false,	false,	true),
-new TableRow( new char[] {                  'e'},	-1,	false,	false,	true),
-new TableRow( new char[] {                  'i'},	14,	false,	false,	true),
-new TableRow( new char[] {                  'i'},	17,	false,	true,	true),
-new TableRow( new char[] {                  'c'},	16,	true,	false,	true),
-new TableRow( new char[] {   'n', 'a', 'b', 'h'},	26,	false,	false,	true),
-new TableRow( new char[] {                  'i'},	18,	false,	false,	true),
-new TableRow( new char[] {                  'i'},	19,	true,	false,	true),
-new TableRow( new char[] {             'o', 'c'},	20,	false,	false,	true),
-new TableRow( new char[] {                  'o'},	22,	false,	false,	false),
-new TableRow( new char[] {                  'c'},	25,	false,	false,	true),
-new TableRow( new char[] {                  'o'},	23,	true,	false,	true),
-new TableRow( new char[] {                  'i'},	24,	true,	false,	true),
-new TableRow( new char[] {             'o', 'c'},	20,	false,	false,	true),
-new TableRow( new char[] {                  'c'},	-1,	false,	false,	true),
-new TableRow( new char[] {                  'n'},	30,	false,	false,	false),
-new TableRow( new char[] {                  'a'},	31,	false,	false,	false),
-new TableRow( new char[] {                  'b'},	32,	false,	false,	false),
-new TableRow( new char[] {                  'h'},	33,	false,	false,	true),
-new TableRow( new char[] {                  'n'},	-1,	true,	false,	true),
-new TableRow( new char[] {                  'a'},	-1,	true,	false,	true),
-new TableRow( new char[] {                  'b'},	-1,	true,	false,	true),
-new TableRow( new char[] {                  'h'},	-1,	true,	false,	true),
-new TableRow( new char[] {                  't'},	-1,	true,	false,	true)
-        };
-
-
+        ParsTable parsTable = new ParsTable(Grammar.LoadFromFile("Grammars\\record.txt"));
 
         public LL1Analyzer(string _exp)
         {
-            exp = _exp;
+            program = _exp;
             //создаем лексический анализатор, на вход подаем текст
             myParser = new Lexan(_exp);
             //Token token;
@@ -88,10 +35,9 @@ new TableRow( new char[] {                  't'},	-1,	true,	false,	true)
         }
 
         //читает символ;
-        private AlalysisStage.TokenTypes readSym()
+        private Symbol readSym()
         {
-            //return exp[symIndex++];
-            return myParser.GetToken().type;
+            return new Symbol( myParser.GetToken().type.ToString().ToLower() );
         }
 
         public bool Check()
@@ -100,11 +46,11 @@ new TableRow( new char[] {                  't'},	-1,	true,	false,	true)
             int i = 0;//номер строки таблицы разбора
             Stack<int> S = new Stack<int>();
             bool la = true;
-            AlalysisStage.TokenTypes sym = readSym();
-            while ((sym != AlalysisStage.TokenTypes.TERMINATOR) && (i != -1))
+            Symbol sym = readSym();
+            while ((sym != Symbol.TERMINATOR) && (i != -1))
             {
                 TableRow row = parsTable[i];
-                if ( Contain(row.terminals,sym) )
+                if ( row.terminals.Contains(sym) )
                 {
                     la = row.accept;
                     if (row.jump==-1)
@@ -143,86 +89,25 @@ new TableRow( new char[] {                  't'},	-1,	true,	false,	true)
         }
 
         //выбрасывает сообщение об ошибке, произошедшей при разборе по строке i
-        private string GetErrMsg(int errRow, AlalysisStage.TokenTypes sym)
+        private string GetErrMsg(int errRow, Symbol sym)
         {
             //throw new Exception("The method or operation is not implemented.");
             //предполагаемые символы
-            char[] suggestedSyms=parsTable[errRow].terminals;
+            Set suggestedSyms=parsTable[errRow].terminals;
             //движемся вниз (по альтернативным продукциям, если они были)
             //и добавляем их terminals
             for (int row = errRow - 1; row > -1; row--)
             {
-                if (!parsTable[row].error) suggestedSyms = Union(suggestedSyms, parsTable[row].terminals);
-                else break;
+                if (!parsTable[row].error) 
+                    suggestedSyms += parsTable[row].terminals;
+                else 
+                    break;
             }
 
-            string errMsg = "имеем " + SymToString(GetCharForTokenType(sym)) + 
-                " ожидается ";
-            for (int i = 0; i < suggestedSyms.Length; i++)
-            {
-                errMsg+=" " + SymToString(suggestedSyms[i]);
-            }
+            string errMsg = "имеем " + sym +
+                " ожидается " + suggestedSyms;
+
             return errMsg;
-        }
-
-        private string SymToString(char p)
-        {
-            char[] syms ={ 'r', 'e', 's', 'c', 'i', 'o', 'n', 'a', 'b', 'h','t'};
-            string[] eqTypes ={ "record", "end",";",":","<идентификатор>",
-                ",","integer", "real","boolean", "char","<конец>"};
-            // TODO возможно, избавиться от линейного поиска
-            //return syms[Array.BinarySearch(eqTypes, tokentype)];
-            for (int i = 0; i < eqTypes.Length; i++)
-            {
-                if (syms[i] == p) return eqTypes[i];
-            }
-            throw new Exception("Не могу преобраовать char в текст");
-        }
-
-        //объединяет пару множеств
-        public char[] Union(char[] setOne, char[] setTwo)
-        {
-            List<char> resultSet = new List<char>();
-            for (int i = 0; i < setOne.Length; i++)
-            {
-                if (resultSet.BinarySearch(setOne[i]) < 0)
-                    resultSet.Add(setOne[i]);
-            }
-            for (int i = 0; i < setTwo.Length; i++)
-            {
-                if (resultSet.BinarySearch(setTwo[i]) < 0)
-                    resultSet.Add(setTwo[i]);
-            }
-            return resultSet.ToArray();
-        }
-        private bool Contain(char[] p, AlalysisStage.TokenTypes tokentype)
-        {
-            char sym = GetCharForTokenType(tokentype);
-
-            for (int symIndex = 0; symIndex < p.Length; symIndex++)
-            {
-                if (p[symIndex] == sym) return true;
-            }
-            return false;
-        }
-
-        private char GetCharForTokenType(AlalysisStage.TokenTypes tokentype)
-        {
-            char[] syms ={ 'r', 'e', 's', 'c', 'i', 'o', 'n','a','b','h', 't'};
-            AlalysisStage.TokenTypes[] eqTypes ={ AlalysisStage.TokenTypes.RECORD, AlalysisStage.TokenTypes.END,
-                                                AlalysisStage.TokenTypes.SEMICOLUMN, AlalysisStage.TokenTypes.COLON,
-                                                AlalysisStage.TokenTypes.IDENT, AlalysisStage.TokenTypes.COMMA,
-                                                AlalysisStage.TokenTypes.INTEGER, AlalysisStage.TokenTypes.REAL,
-                                                AlalysisStage.TokenTypes.BOOLEAN, AlalysisStage.TokenTypes.CHAR,
-                                                AlalysisStage.TokenTypes.TERMINATOR};
-            // TODO возможно, избавиться от линейного поиска
-            //return syms[Array.BinarySearch(eqTypes, tokentype)];
-            for (int i = 0; i < eqTypes.Length; i++)
-            {
-                if (eqTypes[i] == tokentype) return syms[i];
-            }
-
-            throw new Exception("Не могу преобраовать токен в char");
         }
 
     }
